2f9ca8913e62ca4a381156db89babd33c1a5aeb5
Christoffer Magnedal <christoffer@hrdn.se>
2016-11-16 19:21:06 +0100

    First real step

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-test
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Test</title>
+</head>
+<body>
+	Test
+</body>
+</html>
