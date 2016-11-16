07774d3e993cf26c2e57c2959e08dfbeac716157
Christoffer Magnedal <christoffer@hrdn.se>
2016-11-16 19:51:32 +0100

    First blog entry and some basic markup

--- index.php
+++ index.php
@@ -8,6 +8,35 @@
 	<title>Test</title>
 </head>
 <body>
-	Test
+	<header>
+		<h1>The iterative blog</h1>	
+		<p>This is a blog about the development of this blog.</p>
+	</header>
+	<article>
+		<header>
+			<h2>Baby steps towards something new</h2>
+			<span>Published <time>2016-11-16 19:22</time>.</span>
+		</header>
+		<div>
+			<p>The intention with this blog is to document the development of a platform, or something of that effect, for blogging. 
+				It will strive to be as close as possible to the MVP (Minimum Viable Product), without any bloat or feature creep. 
+				What I mean with that is that no feature will be added, unless it is needed at that moment, nothing will be loaded, unless it is necessary for a better product, a better blogging experience or a better use experience.
+			</p>
+			<p>This project aims to be a challenge for me: limiting which features I include in my projects. As a developer it is very easy to let the features pile on, until you got something that is many many times greater than what you imagined from the beginning. I find that adding a lot of features to the todo list makes it a lot harder just to finish one of the items. So this will be a challenge to do the opposite, limit them as much as possible. <br>
+				What I hope that this will mean for me in the long run is a better focus on the main problem that I am trying to solve, instead of growing the project in size and difficulty without any real use.
+			</p>
+			<p>
+				At this moment, there is nothing but a very basic html page here. Nothing dynamic, no CSS, no Javascript, nothing like that. In the end, I hope to end up with a product; where I can easily publish a blogpost, where I do not have to write all the actual html by hand, that looks good, that is responsive, that loads fast and everything else that you would hope every page would have.
+			</p>
+			<p>I hope that I do not end up with bloat, slow loading or anything other negative.</p>
+		</div>
+		<footer>
+			<p>Time to write: <time>28 minutes</time>.</p>
+		</footer>
+	</article>
+	<footer>
+		<p>Want the code for this project? Please visit my Github-page at <a href="https://github.com/CCHolmgren/theiterativeblog">ccholmgren/theiterativeblog</a>.</p>
+		<p>Want to know more about me? You can find my personal page at <a href="http://christofferholmgren.se/">http://christofferholmgren.se/</a>. It might be a little outdated, but it provides a decent overview over what I have accomplished so far!</p>
+	</footer>
 </body>
 </html>
